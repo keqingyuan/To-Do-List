db71788c539b17a835a5fc75c7d0cf3de2679c83
Onion <kaka@ThomasKetekiMacBook-Pro.local>
2015-01-07 22:09:54 +0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Onion on 15-1-7.
+//  Copyright (c) 2015年 tak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Onion on 15-1-7.
+//  Copyright (c) 2015年 tak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
